60f604511a64900bfc9fd5ae8cec8635e49c098d
olafdev1021 <olafdev1021@gmail.com>
2024-12-22 01:30:12 -0600

    olaf
    commited the crud with authenticated.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,55 @@
+class ProductsController < ApplicationController
+
+  before_action :set_product, only: %i[ show edit update destroy]
+  allow_unauthenticated_access only: %i[ index show ]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+   # @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, state: :unprocessable_entity
+    end
+  end
+
+
+  def edit
+    #@product= Product.find(params[:id])
+  end
+
+  def update
+    #@product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, state: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+    def set_product
+      @product= Product.find(params[ :id])
+    end
+
+    def product_params
+      params.expect(product: [:name, :description])
+    end
+ 
+end
